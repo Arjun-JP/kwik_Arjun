0e16c52d64aede30c7f9350477411995b77705ee
Roopa M <roopamohankumar21@gmail.com>
2025-01-22 18:12:51 +0530

    color

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const textColor1 = Color(0xffA7B5FF);
+  static const textColor1 = Color(0xFFA7B5FF);
   static const textColor2 = Color(0xffFC7C1F);
   static const textColorblue = Color(0xFF612ACE);
   static const textpurplelite = Color(0xffCBB4F7);
   static const textColorWhite = Color.fromARGB(255, 255, 255, 255);
   static const textColorblack = Color.fromARGB(255, 0, 0, 0);
-  static const textColorGrey = Color(0xff6F6F6F);
+ 
 
   static const textColorDimGrey = Color(0xffC8C3C3);
   static const textColorSettings = Color(0xff5923C4);
