c63f36f901a094d6a9829a89842affe0ddf1dac8
Roopa M <roopamohankumar21@gmail.com>
2025-01-22 18:08:33 +0530

    firebase setup

---
+++ lib/constants/colors.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const textColor1 = Color(0xffA7B5FF);
+  static const textColor2 = Color(0xffFC7C1F);
+  static const textColorblue = Color(0xFF612ACE);
+  static const textpurplelite = Color(0xffCBB4F7);
+  static const textColorWhite = Color.fromARGB(255, 255, 255, 255);
+  static const textColorblack = Color.fromARGB(255, 0, 0, 0);
+  static const textColorGrey = Color(0xff6F6F6F);
+
+  static const textColorDimGrey = Color(0xffC8C3C3);
+  static const textColorSettings = Color(0xff5923C4);
+  static const dotColorSelected = Colors.black;
+  static const dotColorUnSelected = Color(0xff6F6F6F);
+  static const buttonwhite = Color.fromARGB(255, 255, 255, 255);
+  static const buttonblue = Color(0xFF612ACE);
+  static const buttongrey = Color(0x78788033);
+
+  static const backgroundpurple = Color(0xff995CDD);
+  static const backgrountdarkpurple = Color(0xff612ACE);
+
+  static const primaryColor = Color(0xff612ACE);
+  static const secondaryColor = Color(0xffCEBEFB);
+  static const tertiarycolor = Color(0xff612ACE);
+  static const koffwhiteColor = Color(0xffECECEC);
+  static const kwhiteColor = Colors.white;
+  static const kpurple = Color(0xff612ACE);
+  static const kblackColor = Colors.black;
+  static const kgreyColor = Color(0xff838383);
+  static const kgreyColorlite = Color(0xffC8C3C3);
+  static const secondaryBackgroundColor = Color.fromRGBO(255, 255, 255, 0.74);
+  static const choicechipSelected = Color(0xFF612ACE);
+  static const choicechipUnSelected = Color.fromARGB(255, 255, 255, 255);
+  static const sliderColor = Color(0xFF612ACE);
+  static const sliderBackground = Color.fromARGB(255, 255, 255, 255);
+  static const iconColor = Color(0xff612ACE);
+
+  static const buttonColor1 = Color(0xff5923C4);
+  static const buttonColor2 = Color(0xffAF52DE);
+  static const outerButtonColor1 = Color(0xffAC8BEC);
+  static const outerButtonColor2 = Color(0xff00FFF0);
+  static const cardColor = Color(0xffECECEC);
+  static const cardColorRetell = Color(0xffFFFEE733);
+  static const audioPlayColor = Color(0xffF3ACFF);
+
+  static const bottomSheetBackground = Color.fromARGB(255, 242, 242, 247);
+
+  static const likeButtonColor = Color(0xffF47C25);
+
+  static const snackBarBackground = Color.fromARGB(255, 231, 201, 249);
+}
---
+++ lib/constants/textstyle.dart
@@ -0,0 +1,189 @@
+import 'package:flutter/material.dart';
+import 'package:kwik/constants/colors.dart';
+import 'package:kwik/modules/scaler_module.dart';
+
+
+// Custom TextStyle method
+TextStyle getTextStyle({
+  required ScalerModule scaler,
+  required double fontSize,
+  required FontWeight weight,
+  required Color color,
+  double? letterSpacing,
+}) {
+  return TextStyle(
+    fontSize: scaler.scaleFontSize(fontSize),
+    fontWeight: weight,
+    color: color,
+    letterSpacing: letterSpacing,
+    fontFamily: 'SF-Pro-Rounded',
+  );
+}
+
+// App Theme
+ThemeData appTheme(BuildContext context) {
+  final ScalerModule scaler = ScalerModule(context);
+  return ThemeData(
+    snackBarTheme: const SnackBarThemeData(
+      backgroundColor: Color.fromARGB(
+          255, 231, 201, 249), // Set your desired background color
+      contentTextStyle: TextStyle(
+        color: AppColors
+            .primaryColor, // Set the text color for the SnackBar content
+        fontSize: 16,
+      ),
+      actionTextColor: Colors.yellow, // Set action text color
+    ),
+    useMaterial3: true,
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.secondaryBackgroundColor,
+    textTheme: TextTheme(
+      displayLarge: getTextStyle(
+          scaler: scaler,
+          fontSize: 80,
+          weight: FontWeight.w800,
+          color: const Color(0xFF612ACE)),
+      displayMedium: getTextStyle(
+          scaler: scaler,
+          fontSize: 45,
+          weight: FontWeight.w600,
+          color: AppColors.textColorblack),
+      displaySmall: getTextStyle(
+          scaler: scaler,
+          fontSize: 36,
+          weight: FontWeight.w400,
+          color: AppColors.textColorblack),
+      headlineLarge: getTextStyle(
+          scaler: scaler,
+          fontSize: 32,
+          weight: FontWeight.w800,
+          color: AppColors.textColorblack),
+      headlineMedium: getTextStyle(
+          scaler: scaler,
+          fontSize: 28,
+          weight: FontWeight.w600,
+          color: const Color(0xFF612ACE)),
+      headlineSmall: getTextStyle(
+          scaler: scaler,
+          fontSize: 20,
+          weight: FontWeight.w400,
+          color: AppColors.textColorblack),
+      titleLarge: getTextStyle(
+          scaler: scaler,
+          fontSize: 22,
+          weight: FontWeight.w600,
+          color: AppColors.textColorblack),
+      titleMedium: getTextStyle(
+          scaler: scaler,
+          fontSize: 16,
+          letterSpacing: 0.15,
+          weight: FontWeight.w500,
+          color: AppColors.textColorblack),
+      titleSmall: getTextStyle(
+          scaler: scaler,
+          fontSize: 14,
+          color: AppColors.textColorblack,
+          letterSpacing: 0.1,
+          weight: FontWeight.w500),
+      bodyLarge: getTextStyle(
+          scaler: scaler,
+          fontSize: 16,
+          letterSpacing: 0.15,
+          weight: FontWeight.w800,
+          color: AppColors.textColorblack),
+      bodyMedium: getTextStyle(
+          scaler: scaler,
+          fontSize: 20,
+          letterSpacing: 0.25,
+          weight: FontWeight.w400,
+          color: AppColors.textColorblack),
+      bodySmall: getTextStyle(
+          scaler: scaler,
+          fontSize: 12,
+          letterSpacing: 0.4,
+          weight: FontWeight.w300,
+          color: AppColors.textColorblack),
+      labelLarge: getTextStyle(
+          scaler: scaler,
+          fontSize: 14,
+          letterSpacing: 0.1,
+          color: AppColors.textColorblack,
+          weight: FontWeight.w500),
+      labelMedium: getTextStyle(
+          scaler: scaler,
+          fontSize: 12,
+          color: AppColors.textColorblack,
+          letterSpacing: 0.5,
+          weight: FontWeight.w500),
+      labelSmall: getTextStyle(
+          scaler: scaler,
+          color: AppColors.textColorblack,
+          fontSize: 11,
+          letterSpacing: 0.5,
+          weight: FontWeight.w500),
+    ),
+    appBarTheme: AppBarTheme(
+      color: AppColors.secondaryBackgroundColor,
+      titleTextStyle: TextStyle(
+        color: AppColors.textColorblack,
+        fontSize: scaler.scaleFontSize(20.0),
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.transparent,
+        foregroundColor: AppColors.textColorblack,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+      ),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: AppColors.primaryColor,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(5)),
+        ),
+      ),
+    ),
+    iconButtonTheme: IconButtonThemeData(
+      style: IconButton.styleFrom(
+        // backgroundColor: AppColors.textColorWhite,
+        foregroundColor: AppColors.textColorblack,
+      ),
+    ),
+    cardColor: AppColors.secondaryColor,
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.secondaryBackgroundColor,
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:
+            const BorderSide(color: AppColors.secondaryColor, width: 2.5),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8),
+        borderSide:
+            const BorderSide(color: AppColors.secondaryColor, width: 2.5),
+      ),
+    ),
+    iconTheme: const IconThemeData(color: AppColors.textColorblack),
+    floatingActionButtonTheme: const FloatingActionButtonThemeData(
+      backgroundColor: AppColors.primaryColor,
+      foregroundColor: AppColors.textColorblack,
+    ),
+    colorScheme: const ColorScheme(
+      brightness: Brightness.dark,
+      primary: AppColors.primaryColor,
+      onPrimary: AppColors.textColorblack,
+      secondary: AppColors.secondaryColor,
+      onSecondary: AppColors.textColorblack,
+      error: Color(0xFFE57373),
+      onError: Color(0xFFE53935),
+      background: AppColors.secondaryBackgroundColor,
+      onBackground: AppColors.textColorblack,
+      surface: AppColors.secondaryBackgroundColor,
+      onSurface: AppColors.textColorblack,
+    ),
+  );
+}
